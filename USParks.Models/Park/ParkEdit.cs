7918cb88c751828d0f82710c9e8f207d927d6fec
Ethan Starling <ethan.jr.starling@gmail.com>
2022-05-05 16:36:43 -0400

    added full crud for park

--- USParks.Models/Park/ParkEdit.cs
+++ USParks.Models/Park/ParkEdit.cs
@@ -15,16 +15,17 @@ namespace USParks.Models.Park
         public int ParkId { get; set; }
         [MaxLength(50, ErrorMessage = "There are too many characters in this field.")]
         public string Name { get; set; }
-        [Display(Name = "Park Type")]
+        [Display(Name = "Park Type (national or state)")]
         public ParkType parkType { get; set; }
         [MaxLength(5000, ErrorMessage = "You have hit the 5000 character limit.")]
         public string Description { get; set; }
         [MaxLength(50, ErrorMessage = "There are too many characters in this field.")]
         public string Location { get; set; }
-        [MaxLength(50, ErrorMessage = "There are too many characters in this field.")]
+        [Display(Name = "Size (square miles)")]
+        [Range(0, 20000, ErrorMessage = "Please enter a number between 0 and 20 thousand.")]
         public int Size { get; set; }
         [Display(Name = "Yearly Visitors")]
-        [MaxLength(50, ErrorMessage = "There are too many characters in this field.")]
+        [Range(0, 200000000, ErrorMessage = "Please enter a number between 0 and 20 million.")]
         public int YearlyVisitors { get; set; }
     }
 }
